2ba2dd4ab199ac34c260e4707c32f4fb5667f4e3
Rembrandt <95456210+chrisnunez@users.noreply.github.com>
2022-03-23 12:33:42 -0700

    JS- got rid of commas with concat

--- Password-Generator/Assets/script.js
+++ Password-Generator/Assets/script.js
@@ -5,23 +5,22 @@ var generateBtn = document.querySelector("#generate");
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numericCase = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-  var specialChar = [' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', ',', '=', '>', '?', '@', '[', ']', '^', '_', '{', '|', '}'];
+  var specialChar = ['!', '#', '$', '%', '&', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '_', '{', '|', '}'];
   
-  // var upperAns;
-  // var lowerAns;
-  // var spcAns;
-  // var numberAns;
+
 
 
 
   // Generates a password prompt when the red button is pressed.
 function generatePassword() {
 
-  // First prompt when button is clicked
-var firstPrompt = prompt('Enter a password between 8-128 characters long');
+  // First prompt when button is clicked   *CHANGE TO BE A NUMBER
+var firstPrompt = prompt('Choose the length of your password between 8-128 characters long. Use numbers.');
+
+
 
   // Validates character prompt length
-if ( firstPrompt.length < 8 || firstPrompt.length > 128 ) {
+if ( firstPrompt < 8 || firstPrompt > 128 ) {
       
   alert('Your characters must be between 8 - 128 characters');
   return;
@@ -44,53 +43,53 @@ if ( firstPrompt.length < 8 || firstPrompt.length > 128 ) {
  }
 // User chooses 4 prompts
  if (secondPrompt && thirdPrompt && fourthPrompt && fifthPrompt){
-    var inputPassword = lowerCase + upperCase +  specialChar + numericCase;
-
+    var inputPassword = lowerCase.concat(upperCase, specialChar, numericCase);
+    
 
 
 // User chooses 3 prompts
  } else if (secondPrompt && thirdPrompt && fourthPrompt){
    
-    var inputPassword = lowerCase + upperCase +  specialChar;
+    var inputPassword = lowerCase.concat(upperCase, specialChar);
 
  } else if (secondPrompt && thirdPrompt && fifthPrompt){
 
-    var inputPassword = lowerCase + upperCase + numericCase;
+    var inputPassword = lowerCase.concat(upperCase, numericCase);
 
  } else if (secondPrompt && fourthPrompt && fifthPrompt) {
 
-  var inputPassword = lowerCase + specialChar + numericCase;
+  var inputPassword = lowerCase.concat(specialChar, numericCase);
 
  } else if (thirdPrompt && fourthPrompt && fifthPrompt) {
 
-  var inputPassword = upperCase + upperCase + numericCase;
+  var inputPassword = upperCase.concat(upperCase, numericCase);
 
 
 
 // User chooses 2 prompts
  } else if (secondPrompt && thirdPrompt) {
 
-  var inputPassword = lowerCase + upperCase;
+  var inputPassword = lowerCase.concat(upperCase);
 
  } else if (secondPrompt && fourthPrompt) {
 
-  var inputPassword = lowerCase + specialChar;
+  var inputPassword = lowerCase.concat(specialChar);
 
  } else if (secondPrompt && fifthPrompt){
 
-  var inputPassword = lowerCase + number;
+  var inputPassword = lowerCase.concat(numericCase);
 
  } else if (thirdPrompt && fifthPrompt){
 
-  var inputPassword = upperCase + numericCase;
+  var inputPassword = upperCase.concat(numericCase);
 
  } else if (thirdPrompt && fourthPrompt) {
 
-  var inputPassword = upperCase + specialChar;
+  var inputPassword = upperCase.concat(specialChar);
 
  } else if (fourthPrompt && fifthPrompt){
 
-  var inputPassword = specialChar + numericCase;
+  var inputPassword = specialChar.concat(numericCase);
 
 
 
@@ -116,16 +115,19 @@ if ( firstPrompt.length < 8 || firstPrompt.length > 128 ) {
 
  }
 
-
-
- var passTest = [];
-
+// Get the users' choice and randomizes and brings the choices together
+ var realPassword = [];
+ console.log(inputPassword);
  for (var i = 0; i < firstPrompt; i++) {
 
-  var passTestTwo = inputPassword[Math.floor(Math.random) * inputPassword.length];
-  return passTestTwo;
+  var randomize = inputPassword[Math.floor(Math.random() * inputPassword.length)];
+
+  realPassword.push(randomize);
+  
  }
 
+
+return realPassword.join("");
 
 }
 
@@ -147,48 +149,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-// use while loop check availability of length
-// 4 sections if users wants choice
-//choices user wants
-// for loop
-//Math random only once
-    
-
-
-// GIVEN I need a new, secure password
-
-// WHEN I click the button to generate a password
-
-
-// Step 1: THEN I am presented with a series of prompts for password criteria
-
-
-// WHEN prompted for password criteria
-// Step 2: THEN I select which criteria to include in the password
-
-
-// WHEN prompted for the length of the password
-// Step 3: THEN I choose a length of at least 8 characters and no more than 128 characters
-
-
-// WHEN asked for character types to include in the password
-// Step 4: THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-
-// WHEN I answer each prompt
-// Step 5: THEN my input should be validated and at least one character type should be selected
-
-
-// WHEN all prompts are answered
-// Step 6: THEN a password is generated that matches the selected criteria
-
-
-// WHEN the password is generated
-// Step 7: THEN the password is either displayed in an alert or written to the page
-
