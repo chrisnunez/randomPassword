9673a87591a2027ca39672aa805c63ead21dfd1a
Rembrandt <95456210+chrisnunez@users.noreply.github.com>
2022-03-22 20:04:27 -0700

    Updated Java. Fixed various errors

--- Password-Generator/Assets/script.js
+++ Password-Generator/Assets/script.js
@@ -6,51 +6,132 @@ var generateBtn = document.querySelector("#generate");
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numericCase = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
   var specialChar = [' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', ',', '=', '>', '?', '@', '[', ']', '^', '_', '{', '|', '}'];
- 
+  
+  // var upperAns;
+  // var lowerAns;
+  // var spcAns;
+  // var numberAns;
+
+
 
   // Generates a password prompt when the red button is pressed.
 function generatePassword() {
 
-// First prompt when button is clicked
-  var firstPrompt = prompt('Enter a password between 8-128 characters long');
-  
-  
+  // First prompt when button is clicked
+var firstPrompt = prompt('Enter a password between 8-128 characters long');
 
-// // Validates character prompt length and asks for next prompt
-  if (firstPrompt.length >= 8 && firstPrompt.length <= 128 ) {
-    var secondPrompt = confirm('Do you want to include an Uppercase letter?');
-    var upperCaseAns = upperCase[(Math.floor(Math.random() * upperCase.length))];
-    } else {
-      alert('Your characters must be between 8 - 128 characters')
-      return;
-    }
+  // Validates character prompt length
+if ( firstPrompt.length < 8 || firstPrompt.length > 128 ) {
+      
+  alert('Your characters must be between 8 - 128 characters');
+  return;
+}
+      var secondPrompt = confirm('Do you want to include a lowercase letter?');
+      var thirdPrompt = confirm('Do you want to include an Uppercase letter?');
+      var fourthPrompt = confirm('Do you want to include a special character?');
+      var fifthPrompt = confirm('Do you want to include a number?')
 
-// Lowercase prompt
-    if (secondPrompt = true) {
-      var lowcaseAns = lowerCase[Math.floor(Math.random() * lowerCase.length)];
-      var thirdPrompt = confirm('Do you want to include a lowercase letter?');
-    }
+// Must include 1 before continuing 
+ while (!secondPrompt && !thirdPrompt && !fourthPrompt && !fifthPrompt) {
 
-// Number prompt
-    if (thirdPrompt = true){
-      var fourthPrompt = confirm('Do you want to include a number?');
-      var numAns = numericCase[Math.floor(Math.random() * numericCase)];
-    }
+          alert('Please select at least one')
+      
+          secondPrompt = confirm('Do you want to include a lowercase letter?');
+          thirdPrompt = confirm('Do you want to include an Uppercase letter?');
+          fourthPrompt = confirm('Do you want to include a special character?');
+          fifthPrompt = confirm('Do you want to include a number?')
 
-// Special character prompt
-    if (fourthPrompt = true){
-      var fifthPrompt = confirm('Do you want to include a special character?');
-      var spcAns = specialChar[Math.floor(Math.random() * specialChar.length)];
-    }
+ }
+// User chooses 4 prompts
+ if (secondPrompt && thirdPrompt && fourthPrompt && fifthPrompt){
+    var inputPassword = lowerCase + upperCase +  specialChar + numericCase;
 
-  return(upperCaseAns + lowcaseAns + spcAns);
+
+
+// User chooses 3 prompts
+ } else if (secondPrompt && thirdPrompt && fourthPrompt){
+   
+    var inputPassword = lowerCase + upperCase +  specialChar;
+
+ } else if (secondPrompt && thirdPrompt && fifthPrompt){
+
+    var inputPassword = lowerCase + upperCase + numericCase;
+
+ } else if (secondPrompt && fourthPrompt && fifthPrompt) {
+
+  var inputPassword = lowerCase + specialChar + numericCase;
+
+ } else if (thirdPrompt && fourthPrompt && fifthPrompt) {
+
+  var inputPassword = upperCase + upperCase + numericCase;
+
+
+
+// User chooses 2 prompts
+ } else if (secondPrompt && thirdPrompt) {
+
+  var inputPassword = lowerCase + upperCase;
+
+ } else if (secondPrompt && fourthPrompt) {
+
+  var inputPassword = lowerCase + specialChar;
+
+ } else if (secondPrompt && fifthPrompt){
+
+  var inputPassword = lowerCase + number;
+
+ } else if (thirdPrompt && fifthPrompt){
+
+  var inputPassword = upperCase + numericCase;
+
+ } else if (thirdPrompt && fourthPrompt) {
+
+  var inputPassword = upperCase + specialChar;
+
+ } else if (fourthPrompt && fifthPrompt){
+
+  var inputPassword = specialChar + numericCase;
+
+
+
+
+  // User chooses 1
+
+ } else if (secondPrompt){
+
+  var inputPassword = lowerCase;
+
+ } else if (thirdPrompt){
+
+  var inputPassword = upperCase;
+
+ } else if (fourthPrompt) {
+
+  var inputPassword = specialChar;
+
+ } else if (fifthPrompt) {
+
+ var inputPassword = numericCase;
+
+
+ }
+
+
+
+ var passTest = [];
+
+ for (var i = 0; i < firstPrompt; i++) {
+
+  var passTestTwo = inputPassword[Math.floor(Math.random) * inputPassword.length];
+  return passTestTwo;
+ }
 
 
 }
-  
-  
-  
-  
+
+
+
+
 
 
 // Write password to the #password input
@@ -67,6 +148,17 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
+
+
+
+// use while loop check availability of length
+// 4 sections if users wants choice
+//choices user wants
+// for loop
+//Math random only once
+    
 
 
 // GIVEN I need a new, secure password
